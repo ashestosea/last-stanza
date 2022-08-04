66a52b3647f0622426602b589533c3ce9d597c55
chronotab <chronotab@proton.me>
2022-08-04 10:34:06 -0500

    Init

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use bevy::prelude::*;
+use bevy_editor_pls::*;
+
+fn main() {
+	App::new()
+	.add_plugins(DefaultPlugins)
+	.add_plugin(EditorPlugin)
+	.add_plugin(bevy::diagnostic::FrameTimeDiagnosticsPlugin)
+	.add_plugin(bevy::diagnostic::EntityCountDiagnosticsPlugin)
+	.run();
+}
