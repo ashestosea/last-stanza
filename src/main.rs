b56c282da3a8553f2b7fe2fb4f8880de2a23c2bb
chronotab <chronotab@proton.me>
2022-08-04 11:07:03 -0500

    Add heron physics and set dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use bevy::prelude::*;
-use bevy_editor_pls::*;
+// use bevy_editor_pls::*;
+use heron::prelude::*;
 
 fn main() {
 	App::new()
 	.add_plugins(DefaultPlugins)
-	.add_plugin(EditorPlugin)
-	.add_plugin(bevy::diagnostic::FrameTimeDiagnosticsPlugin)
-	.add_plugin(bevy::diagnostic::EntityCountDiagnosticsPlugin)
+	// .add_plugin(EditorPlugin)
+	// .add_plugin(bevy::diagnostic::FrameTimeDiagnosticsPlugin)
+	// .add_plugin(bevy::diagnostic::EntityCountDiagnosticsPlugin)
+	.add_plugin(PhysicsPlugin::default())
 	.run();
 }
